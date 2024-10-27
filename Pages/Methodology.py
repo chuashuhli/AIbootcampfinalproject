007346b0f59fb54f15fafe57d282b2813dd288db
chuashuhli <chuashuhli@gmail.com>
2024-10-27 12:22:30 +0800

    first commit

---
+++ Pages/Methodology.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+st.title("Methodology")
+st.write("""
+This page outlines the methodology followed in generating this page.
+The methodology involves the following key steps:
+""")
+
+st.header("1. User Query")
+
+st.header("2. Identify Available Social Assistance")
+
+st.header("3. Get Social Assistance Details")
+
+st.header("4. Generate Response Based on Social Assistance Details")
+
+st.header("5. LLM Response")
+
+st.header("6. Location of Social Service Offices were converted to Latitude and Longitude to be added into the map")
+
+st.write("This structured approach ensures that responses are tailored, accurate, and directly relevant to the user's social assistance needs.")
